50e5067a1aabdf61a7d2c51e3c6b19bc57b0486c
Lernt33 <giorgizautashvili24@gmail.com>
2024-03-02 03:03:03 +0400

    Added language manager, which saves user's language to cookies

--- Back/app.py
+++ Back/app.py
@@ -1,9 +1,27 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,url_for,request,redirect,flash,session
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return render_template()
+def index():
+    # if session['logged_in']:
+    return render_template('index.html')
+@app.route('/mynotes')
+def mynotes():
+    return render_template('mynotes.html')
+
+@app.route('/reg')
+def register():
+    return render_template('reg.html')
+
+@app.route('/log')
+def log():
+    return render_template('log.html')
+@app.route('/logout')
+def logout():
+    return redirect(url_for('index'))
+@app.route('/admin')
+def admin():
+    return render_template('AdminPanel.html')
 
 if __name__ == '__main__':
-    app.run('127.0.0.1',port=80,debug=False)
+    app.run(host='0.0.0.0', port=80)
